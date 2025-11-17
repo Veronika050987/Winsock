e0022d1fa9173da7d1bf123a34eca5487ae127f5
Veronika050987 <veronicaviv@mail.ru>
2025-11-17 19:25:51 +0300

    client ca write messages

--- Server/main.cpp
+++ Server/main.cpp
@@ -13,16 +13,9 @@ using namespace std;
 
 #define DEFAULT_PORT	"27015"
 #define BUFFER_LENGTH	  1460
-#define MAX_CLIENTS			 3
-#define g_sz_SORRY		"Error: Количество подключений превышено"
-#define IP_STR_MAX_LENGTH	16
+#define MAX_CLIENTS			 5
 
-INT n = 0;	//Количество активных клиентов
-SOCKET client_sockets[MAX_CLIENTS] = {};
-DWORD threadIDs[MAX_CLIENTS] = {};
-HANDLE hThreads[MAX_CLIENTS] = {};
-
-VOID WINAPI HandleClient(SOCKET client_socket);
+VOID HandleClient(SOCKET client_socket);
 
 int main()
 {
@@ -98,14 +91,17 @@ int main()
 	}
 
 	//6) Обработка запросов от клиентов:
-
+	INT n = 0;	//Количество активных клиентов
+	SOCKET client_sockets[MAX_CLIENTS] = {};
+	DWORD threadIDs[MAX_CLIENTS] = {};
+	HANDLE hThreads[MAX_CLIENTS] = {};
 	cout << hThreads << endl;
 	cout << HandleClient << endl;
 	cout << "Accept client connections..." << endl;
 	do
 	{
-		SOCKET client_socket = accept(listen_socket, NULL, NULL);
-		/*if (client_sockets[n] == INVALID_SOCKET)
+		client_sockets[n] = accept(listen_socket, NULL, NULL);
+		if (client_sockets[n] == INVALID_SOCKET)
 		{
 			dwLastError = WSAGetLastError();
 			cout << "Accept failed with error: " << dwLastError << endl;
@@ -113,79 +109,32 @@ int main()
 			freeaddrinfo(result);
 			WSACleanup();
 			return dwLastError;
-		}*/
+		}
 		//HandleClient(client_socket);
-		if (n < MAX_CLIENTS)
-		{
-			client_sockets[n] = client_socket;
-			hThreads[n] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)HandleClient, (LPVOID)client_sockets[n], 0, threadIDs + n);
-			n++;
-		}
-		else
-		{
-			CHAR recv_buffer[BUFFER_LENGTH] = {};
-			INT iResult = recv(client_socket, recv_buffer, BUFFER_LENGTH, 0);
-			if (iResult > 0)
-			{
-				cout << "Bytes received: " << iResult << endl;
-				cout << "Message: " << recv_buffer << endl;
-				INT iSendResult = send(client_socket, g_sz_SORRY, strlen(g_sz_SORRY), 0);
-				closesocket(client_socket);
-			}
-		}
+		hThreads[n] = CreateThread(NULL, 0, HandleClient, client_sockets + n, 0, threadIDs + n);
+		n++;
 	} while (true);
-
-	WaitForMultipleObjects(MAX_CLIENTS, hThreads, TRUE, INFINITE);
 
 	closesocket(listen_socket);
 	freeaddrinfo(result);
 	WSACleanup();
 	return dwLastError;
 }
-INT GetSlotIndex(DWORD dwID)
+VOID HandleClient(SOCKET client_socket)
 {
-	for (int i = 0; i < MAX_CLIENTS; i++)
-	{
-		if (threadIDs[i] == dwID)return i;
-	}
-}
-VOID Shift(INT start)
-{
-	for (INT i = start; i < MAX_CLIENTS; i++)
-	{
-		client_sockets[i] = client_sockets[i + 1];
-		threadIDs[i] = threadIDs[i + 1];
-		hThreads[i] = hThreads[i + 1];
-	}
-	client_sockets[MAX_CLIENTS - 1] = NULL;
-	threadIDs[MAX_CLIENTS - 1] = NULL;
-	hThreads[MAX_CLIENTS - 1] = NULL;
-	n--;
-}
-VOID WINAPI HandleClient(SOCKET client_socket)
-{
-	SOCKADDR_IN peer{};
-	CHAR address[IP_STR_MAX_LENGTH] = {};
-	INT address_length = IP_STR_MAX_LENGTH;
-	getpeername(client_socket, (SOCKADDR*)&peer, &address_length);
-	inet_ntop(AF_INET, &peer.sin_addr, address, address_length);
-	INT port = ((peer.sin_port & 0xFF) << 8) + (peer.sin_port >> 8);
-	cout << address << ":" << port << endl;
-	/////////////////////////////////////////////////////
 	INT iResult = 0;
 	DWORD dwLastError = 0;
 	//7)Получение запросов от клиента:
-	CHAR send_buffer[BUFFER_LENGTH] = "Привет клиент";
-	CHAR recv_buffer[BUFFER_LENGTH] = {};
 	do
 	{
+		CHAR send_buffer[BUFFER_LENGTH] = "Привет клиент";
+		CHAR recv_buffer[BUFFER_LENGTH] = {};
 		INT iSendResult = 0;
-		ZeroMemory(send_buffer, BUFFER_LENGTH);
-		ZeroMemory(recv_buffer, BUFFER_LENGTH);
+
 		iResult = recv(client_socket, recv_buffer, BUFFER_LENGTH, 0);
 		if (iResult > 0)
 		{
-			cout << iResult << " Bytes received from " << address << ":" << port << " - " << recv_buffer << endl;
+			cout << iResult << " Bytes received, Message: " << recv_buffer << endl;
 			iSendResult = send(client_socket, recv_buffer, strlen(recv_buffer), 0);
 			if (iSendResult == SOCKET_ERROR)
 			{
@@ -202,12 +151,6 @@ VOID WINAPI HandleClient(SOCKET client_socket)
 			cout << "Receive failed with error: " << dwLastError << endl;
 			break;
 		}
-	} while (iResult > 0 && !strstr(recv_buffer, "quit"));
-	DWORD dwID = GetCurrentThreadId();
-	Shift(GetSlotIndex(dwID));
-	cout << address << ":" << port << " leaved" << endl;
-	ExitThread(0);
-	//ExitThread(GetExitCodeThread());
+	} while (iResult > 0);
 	closesocket(client_socket);
-
 }
